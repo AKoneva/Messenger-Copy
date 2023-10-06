729d31443f1fcf4132458c06e87778375db4d78c
Anna <35114047+AKoneva@users.noreply.github.com>
2023-10-06 21:01:52 +0900

    init

---
+++ Messenger Copy/Messenger Copy/App/Messenger_CopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_CopyApp.swift
+//  Messenger Copy
+//
+//  Created by Анна Перехрест  on 2023/10/06.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_CopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
