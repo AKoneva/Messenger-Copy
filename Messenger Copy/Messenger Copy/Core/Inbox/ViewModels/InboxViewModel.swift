e93b7cee3be67d788f500e7851e8849e38aee0d1
Anna <35114047+AKoneva@users.noreply.github.com>
2023-10-14 15:07:03 +0900

    add removing functionality

--- Messenger Copy/Messenger Copy/Core/Inbox/ViewModels/InboxViewModel.swift
+++ Messenger Copy/Messenger Copy/Core/Inbox/ViewModels/InboxViewModel.swift
@@ -13,42 +13,59 @@ import Combine
 class InboxViewModel: ObservableObject {
     @Published var currentUser: User?
     @Published var recentMessages = [Message]()
-    
+
     private var cancelable = Set<AnyCancellable>()
-    
+
     let service = InboxService()
-    
+
     init() {
         setUpSubscribers()
         service.observeRecentMessege()
     }
-    
+
     private func setUpSubscribers() {
         UserService.shared.$currentUser.sink { [weak self] userFromUserService in
             self?.currentUser = userFromUserService
         }.store(in: &cancelable)
-        
+
         service.$documentChanges.sink { [weak self] changes in
             self?.loadInitialMessages(from: changes)
         }.store(in: &cancelable)
     }
-    
+
+    @MainActor
     private func loadInitialMessages(from changes: [DocumentChange]) {
-        var messages = changes.compactMap({ try? $0.document.data(as: Message.self) })
-        
-        for message in messages {
-            UserService.fetchUser(withUid: message.chatPartherID) { user in
-                var settedUserMessage = message
-                settedUserMessage.user = user
-                
-                if let index = self.recentMessages.firstIndex(where: { $0.chatPartherID == settedUserMessage.user?.id }) {
-                    self.recentMessages[index] = settedUserMessage
-                } else {
-                    self.recentMessages.append(settedUserMessage)
-                }
-                
-                self.recentMessages.sort(by: { $0.timeStamp > $1.timeStamp })
+        for change in changes {
+            switch change.type {
+                case .added, .modified:
+                    if let message = try? change.document.data(as: Message.self) {
+                        UserService.fetchUser(withUid: message.chatPartherID) { user in
+                            var settedUserMessage = message
+                            settedUserMessage.user = user
+
+                            if let index = self.recentMessages.firstIndex(where: { $0.chatPartherID == settedUserMessage.user?.id }) {
+                                self.recentMessages[index] = settedUserMessage
+                            } else {
+                                self.recentMessages.append(settedUserMessage)
+                            }
+                        }
+                    }
+                case .removed:
+                    if let message = try? change.document.data(as: Message.self) {
+                        recentMessages.removeAll { $0.chatPartherID == message.chatPartherID }
+                    }
             }
         }
+
+        recentMessages.sort(by: { $0.timeStamp > $1.timeStamp })
+    }
+
+
+    func deleteChat(with user: User?) {
+        guard let user = user else { return }
+
+        let chatService = ChatService(chatParther: user)
+
+        chatService.deleteChat()
     }
 }
